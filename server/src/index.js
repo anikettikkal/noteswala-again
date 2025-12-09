0982bc416a36dc7279d7cfb52fce6d179983d8ad
anikettikkal <tikkalaniket2019@gmail.com>
2025-12-09 22:51:02 +0530

    mongodb connected successfully

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,18 @@
-import express from "express"
+import connectDB from "../src/db/index.js";
+import app from "./app.js";
+import dotenv from 'dotenv';
 
-const app = express();
-
-app.get('/',(req,res)=>{
-    res.send("hello world")
+dotenv.config({
+    path: './.env'
 })
 
-const port = process.env.PORT || 5000;
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running on port ${process.env.PORT || 8000}`);
+})
+    })
+.catch((err) => {
+    console.log("mongodb connection failed", err);
 
-app.listen(port,()=>{
-    console.log(`app running on port ${port}`);
-    
 })
